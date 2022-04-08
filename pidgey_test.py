8edaa03569b210b7265f3c065db38400404c3659
tfitzner <5036869+tfitzner@users.noreply.github.com>
2022-04-08 12:06:33 -0400

    changed pidgey to all pokemon list

--- pidgey_test.py
+++ pidgey_test.py
@@ -8,25 +8,39 @@ import matplotlib.pyplot as plt
 #HP,Attack,Defense,Sp. Atk,Sp.Def,Speed
 #40,45,40,35,35,56
 
-name ='Pidgey'
-base_hp = 40
-base_attack = 45
-base_defense = 40
-base_sp_attack = 35
-base_sp_defense = 35
-base_speed = 56
+kanto_pokemon = pd.read_csv('pokemon_kanto.csv', index_col=0)
+
+pokemon_list = []
 
 stats_list = ['level', 'hp', 'attack', 'defense', 'sp_attack', 'sp_defense', 'speed']
 
 EV = 0
 Nature = 1
-pidgey_list = []
 
+name = 0
+base_hp = 0
+base_attack = 0
+base_defense = 0
+base_sp_attack = 0
+base_sp_defense = 0
+base_speed = 0
 
-def calculate_wild_stats(base_hp, base_attack, base_defense, base_sp_attack, base_sp_defense, base_speed, EV, Nature):
+def calculate_wild_stats(EV, Nature):
+
+    #generate stats based on pokemon pandas table for random pokemon_list
+    x = random.randint(1, 151)
+
+    #assign values to calulations for base stats
+    name = kanto_pokemon.loc[x]['Name']
+    base_hp = kanto_pokemon.loc[x]['HP']
+    base_attack = kanto_pokemon.loc[x]['Attack']
+    base_defense = kanto_pokemon.loc[x]['Defense']
+    base_sp_attack = kanto_pokemon.loc[x]['Sp.Atk']
+    base_sp_defense = kanto_pokemon.loc[x]['Sp.Def']
+    base_speed = kanto_pokemon.loc[x]['Speed']
 
     #generate the random levels of an area
-    Level = random.randint(2,100)
+    Level = random.randint(1,50)
 
     #generate the IV possible which is 1 to 31 inclusive low, high
     IV = []
@@ -49,20 +63,24 @@ def calculate_wild_stats(base_hp, base_attack, base_defense, base_sp_attack, bas
     #return array of stats for pokemon with level
     return [Level, HP, attack, defense, sp_attack, sp_defense, speed]
 
-for i in range(100):
-    pidgey_list.append(calculate_wild_stats(base_hp, base_attack, base_defense, base_sp_attack, base_sp_defense, base_speed, EV, Nature))
+for i in range(10000):
+
+    pokemon_list.append(calculate_wild_stats(EV, Nature))
 
 
-df = pd.DataFrame(pidgey_list, columns=stats_list)
-print(df)
+
+df = pd.DataFrame(pokemon_list, columns=stats_list)
+
+print(df.std(ddof=0))
 
 fig, ax = plt.subplots()
 
-
-
 ax.scatter(df.hp, df.level, color='b', label='HP')
-ax.scatter(df.attack, df.level, color='r', label='attack')
-ax.scatter(df.defense, df.level, color='y', label='defense')
+#ax.scatter(df.attack, df.level, color='r', label='attack')
+#ax.scatter(df.defense, df.level, color='y', label='defense')
+#ax.scatter(df.sp_attack, df.level, color='purple', label='sp. attack')
+#ax.scatter(df.sp_defense, df.level, color='orange', label='sp. defense')
+#ax.scatter(df.speed, df.level, color='pink', label='speed')
 
 ax.legend()
 ax.grid(True)
