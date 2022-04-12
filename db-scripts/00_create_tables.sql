cf786e4c29cb940cf2936f91e2a648d395a18a62
Nate <101949953+evokercodex@users.noreply.github.com>
2022-04-11 22:50:10 -0400

    Update 00_create_tables.sql

--- db-scripts/00_create_tables.sql
+++ db-scripts/00_create_tables.sql
@@ -1,12 +1,27 @@
 /*************************************\
+** GAME
+\*************************************/
+DROP TABLE IF EXISTS game;
+CREATE TABLE game (
+	game_id INTEGER NOT NULL
+	,name TEXT NOT NULL
+	,release_date TEXT NOT NULL
+	,platform TEXT NOT NULL
+	,generation TEXT NOT NULL
+	,PRIMARY KEY(game_id AUTOINCREMENT)
+);
+
+
+/*************************************\
 ** POKEMON
 \*************************************/
 DROP TABLE IF EXISTS pokemon;
 CREATE TABLE pokemon (
 	pokemon_id INTEGER NOT NULL
+	,ndex INTEGER NOT NULL
 	,name TEXT NOT NULL
 	,type_1 TEXT NOT NULL
-	,type_2 TEXT
+	,type_2 TEXT NULL
 	,total INTEGER NOT NULL
 	,hp	INTEGER NOT NULL
 	,attack	INTEGER NOT NULL
@@ -14,10 +29,11 @@ CREATE TABLE pokemon (
 	,sp_attack INTEGER NOT NULL
 	,sp_defense	INTEGER NOT NULL
 	,speed INTEGER NOT NULL
-	,generation	INTEGER NOT NULL
-	,is_legendary INTEGER NOT NULL
+	,generation	INTEGER NOT NULL DEFAULT '?'
+	,is_legendary INTEGER NOT NULL DEFAULT 0
 	,PRIMARY KEY(pokemon_id AUTOINCREMENT)
 );
+
 
 /*************************************\
 ** NATURE
@@ -43,6 +59,18 @@ CREATE TABLE region (
 	,name TEXT NOT NULL
 	,PRIMARY KEY(region_id AUTOINCREMENT)
 );
+
+
+/*************************************\
+** REGION_GEN_XREF
+\*************************************/
+DROP TABLE IF EXISTS region;
+CREATE TABLE region (
+	region_id INTEGER NOT NULL
+	,name TEXT NOT NULL
+	,PRIMARY KEY(region_id AUTOINCREMENT)
+);
+
 
 /*************************************\
 ** ROUTE
