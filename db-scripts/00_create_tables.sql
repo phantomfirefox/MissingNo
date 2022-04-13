7a237a8ad4f98ac846ff2a9de6b83a6a0cbb00d3
Nate <101949953+evokercodex@users.noreply.github.com>
2022-04-12 20:30:27 -0400

    Add files via upload

--- db-scripts/00_create_tables.sql
+++ db-scripts/00_create_tables.sql
@@ -1,3 +1,27 @@
+PRAGMA foreign_keys = OFF;
+
+/*************************************\
+** PLATFORM
+\*************************************/
+DROP TABLE IF EXISTS platform;
+CREATE TABLE platform (
+	platform_id INTEGER NOT NULL
+	,name TEXT NOT NULL
+	,PRIMARY KEY(platform_id AUTOINCREMENT)
+);
+
+
+/*************************************\
+** GENERATION
+\*************************************/
+DROP TABLE IF EXISTS generation;
+CREATE TABLE generation (
+	generation_id INTEGER NOT NULL
+	,name TEXT NOT NULL
+	,PRIMARY KEY(generation_id AUTOINCREMENT)
+);
+
+
 /*************************************\
 ** GAME
 \*************************************/
@@ -6,9 +30,11 @@ CREATE TABLE game (
 	game_id INTEGER NOT NULL
 	,name TEXT NOT NULL
 	,release_date TEXT NOT NULL
-	,platform TEXT NOT NULL
-	,generation TEXT NOT NULL
+	,platform_id TEXT NOT NULL
+	,generation_id TEXT NOT NULL
 	,PRIMARY KEY(game_id AUTOINCREMENT)
+	,FOREIGN KEY(platform_id) REFERENCES platform(platform_id)
+	,FOREIGN KEY(generation_id) REFERENCES generation(generation_id)
 );
 
 
@@ -29,9 +55,10 @@ CREATE TABLE pokemon (
 	,sp_attack INTEGER NOT NULL
 	,sp_defense	INTEGER NOT NULL
 	,speed INTEGER NOT NULL
-	,generation	INTEGER NOT NULL DEFAULT '?'
+	,generation_id	INTEGER NOT NULL DEFAULT 0
 	,is_legendary INTEGER NOT NULL DEFAULT 0
 	,PRIMARY KEY(pokemon_id AUTOINCREMENT)
+	,FOREIGN KEY(generation_id) REFERENCES generation(generation_id)	
 );
 
 
@@ -49,6 +76,7 @@ CREATE TABLE nature (
 	,speed INTEGER NOT NULL
 	,PRIMARY KEY(nature_id AUTOINCREMENT)
 );
+
 
 /*************************************\
 ** REGION
@@ -81,3 +109,40 @@ CREATE TABLE route (
 	,name TEXT NOT NULL
 	,PRIMARY KEY(route_id AUTOINCREMENT)
 );
+
+
+/*************************************\
+** REGION_ROUTE_XREF
+\*************************************/
+DROP TABLE IF EXISTS region_route_xref;
+CREATE TABLE region_route_xref (
+	region_route_xref_id INTEGER NOT NULL
+	,region_id INTEGER NOT NULL
+	,route_id INTEGER NOT NULL
+	,game_id INTEGER NOT NULL
+	,PRIMARY KEY(region_route_xref_id AUTOINCREMENT)
+	,FOREIGN KEY(region_id) REFERENCES region(region_id)
+	,FOREIGN KEY(route_id) REFERENCES route(route_id)	
+	,FOREIGN KEY(game_id) REFERENCES game(game_id)		
+);
+
+
+/*************************************\
+** REGION_ROUTE_XREF
+\*************************************/
+DROP TABLE IF EXISTS pokemon_spawn_rate;
+CREATE TABLE pokemon_spawn_rate (
+	pokemon_spawn_rate_id INTEGER NOT NULL 
+	,pokemon_id INTEGER NOT NULL
+	,region_route_xref_id INTEGER NOT NULL
+	,location TEXT NOT NULL
+	,min_level INTEGER NOT NULL
+	,max_level INTEGER NULL
+	,rate NUMERIC(3,3) NOT NULL
+	,PRIMARY KEY(pokemon_spawn_rate_id AUTOINCREMENT)
+	,FOREIGN KEY(pokemon_id) REFERENCES pokemon(pokemon_id)	
+	,FOREIGN KEY(region_route_xref_id) REFERENCES region_route_xref(region_route_xref_id)			
+);
+
+
+PRAGMA foreign_keys = ON;
